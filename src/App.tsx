b81bebcc3921ccf8b14b3a3847f9e557a3c90a65
Gabriela Rodrigues <gabrielarodrigues432@gmail.com>
2021-09-09 19:27:14 -0300

    (feat)::providers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Routes from "./Routes";
-import GlobalStyle from "./Global/styles";
 
 function App() {
   return (
     <div>
       <Routes />
-      <GlobalStyle />
     </div>
   );
 }
