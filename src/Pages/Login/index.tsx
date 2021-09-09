c35119911e33bf7399e1184597cb3282e24150a7
André Kuratomi <andrekuratomi@gmail.com>
2021-09-09 09:38:23 -0300

    feat(logic part drafted): typed libs added, Register and Login pages logic part drafted

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,6 +1,78 @@
+import { useHistory, Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+// import { FiUser, FiLock } from "react-icons/fi";
+
 export const Login = () => {
-    return(
-        <>
-        </>
-    )
+  interface ILogin {
+    data: string;
+  }
+
+  interface ILoginValues {
+    username: string;
+    password: string;
+  }
+
+  const formSchema = yup.object().shape({
+    username: yup
+      .string()
+      .required("Required input!")
+      .min(4, "Minimum 4 characters!"),
+    password: yup
+      .string()
+      .required("Required input!")
+      .min(8, "Minimum 8 characters!"),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginValues>({
+    resolver: yupResolver(formSchema),
+  });
+  const history = useHistory();
+  const submitFunction = ({ data }: ILogin) => {
+    console.log(data);
+    history.push("/dashboard");
+  };
+
+  return (
+    <>
+      {/* <Header /> */}
+      <header>
+        <figure>
+          <img src="../assets/img/logo" alt="Logo" />
+          <figcaption>Logo</figcaption>
+        </figure>
+        <h3>MovieRater</h3>
+        <nav>
+          <Link to="/aboutus">Sobre Nós</Link>
+          <Link to="/singIn">Entrar</Link>
+          <Link to="/singUp">Junte-se</Link>
+        </nav>
+      </header>
+      <form onSubmit={handleSubmit(submitFunction)}>
+        <input
+          //   FiUser
+          placeholder="Usuário"
+          {...register("username")}
+        />
+        {errors.username?.message}
+        <input
+          // FiLock
+          placeholder="Senha"
+          {...register("password")}
+        />
+        {errors.password?.message}
+        <button type="submit">Entrar</button>
+      </form>
+      <p>
+        <Link to="/signUp">Registrar</Link>
+      </p>
+      <p>
+        <Link to="/">Voltar para a página principal</Link>
+      </p>
+    </>
+  );
 };
