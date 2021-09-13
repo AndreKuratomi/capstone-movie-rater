506368345a9b22e4482cf6694bbb3030d47b508b
Carlos Lima <hiro1kz77@gmail.com>
2021-09-13 18:35:19 -0300

    fix(component): fixed jwt bug conflict

--- src/Providers/Movies/index.tsx
+++ src/Providers/Movies/index.tsx
@@ -46,8 +46,8 @@ export const MoviesProvider = ({ children }: IMovies) => {
   const [movies, setMovies] = useState([]);
   const [searchedMovies, setSearchedMovies] = useState([]);
   const [aboutMovie, setAboutMovie] = useState({});
-  const decode = jwtDecode<JwtPayload>(token);
-  console.log(decode.sub);
+  // const decode = jwtDecode<JwtPayload>(token);
+  // console.log(decode.sub);
   const getMovies = (page: number) => {
     api
       .get(`movies?page=${page}`)
@@ -62,21 +62,8 @@ export const MoviesProvider = ({ children }: IMovies) => {
   };
   const AddToFavorites = (data: IMoviesList) => {
     const Addedmovie = {
-      userId: Number(decode.sub),
-      adult: data.adult,
-      backdrop_path: data.backdrop_path,
-      genre_ids: data.genre_ids,
-      original_language: data.original_language,
-      original_title: data.original_title,
-      overview: data.overview,
-      popularity: data.popularity,
-      poster_path: data.poster_path,
-      release_date: data.release_date,
-      title: data.title,
-      video: data.video,
-      vote_average: data.vote_average,
-      vote_count: data.vote_count,
-      id: data.id,
+      userId: Number(1),
+      ...data
     };
 
     api
